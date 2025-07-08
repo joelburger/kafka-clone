f3cb19b57cd6d75764c5f912bfb47d703e389f0f
joel <joel@burgerjoe.dev>
2025-07-07 22:42:20 -0600

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -4,6 +4,21 @@ console.log('Logs from your program will appear here!');
 
 const server: net.Server = net.createServer((connection: net.Socket) => {
   // Handle connection
+
+  connection.on('data', (data: Buffer) => {
+    console.log('Received data: ', data);
+
+    const buffer: Buffer = Buffer.alloc(8);
+    buffer.writeUint32BE(0);
+    buffer.writeUint32BE(7, 4);
+    console.log('Buffer', buffer);
+
+    connection.write(buffer);
+  });
+
+  connection.on('end', () => {
+    console.log('Client disconnected');
+  });
 });
 
 server.listen(9092, '127.0.0.1');
